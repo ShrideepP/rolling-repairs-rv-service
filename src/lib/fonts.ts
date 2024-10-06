81a13f1d64cabd392215bc38d63e2d3a320cbb07
ShrideepP <shrideeppakhare2005@gmail.com>
2024-10-06 11:08:15 +0530

    first commit

---
+++ src/lib/fonts.ts
@@ -0,0 +1,14 @@
+import { Roboto_Condensed } from "next/font/google";
+import localFont from "next/font/local";
+
+export const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--roboto-condensed",
+});
+
+export const riftBold = localFont({
+  src: "../assets/fonts/Rift-Bold.otf",
+  display: "swap",
+  variable: "--rift-bold",
+});
